7b1f8ca60b187cc6ee919bc6d3acc114f20f2b7c
MshSiam <siyamkenway@gmail.com>
2021-08-15 22:31:59 +0600

    half complete

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('login-submit').addEventListener('click', function(){
+
+    // get email
+    const emailField = document.getElementById('user-email')
+    const userEmail = emailField.value
+    
+
+    // get password
+    const emailPassword = document.getElementById('user-password')
+    const userPassword = emailPassword.value
+    
+
+    // check email & redirect
+    if(userEmail == '@gmail.com' && userPassword == 'password'){
+        window.location.href = 'bank.html'
+    }else{
+        console.log('Invalid User')
+    }
+})
+
+
+
+// deposite & withdraw

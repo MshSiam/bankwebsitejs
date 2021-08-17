55668b1fd1f3d626130a06d8bbd739446dc045ad
MshSiam <siyamkenway@gmail.com>
2021-08-17 07:00:59 +0600

    deposite calculation function add

--- banking.js
+++ banking.js
@@ -1,15 +1,44 @@
-// deposite button event
+// // // deposite button event // // //
+
 document.getElementById('deposite-button').addEventListener('click', function(){
     // get the amount deposite
     const depositeInput = document.getElementById('deposite-input')
-    const depositeAmount = depositeInput.value
+    const newDepositeAmountText = depositeInput.value 
+    const newDepositeAmount = parseFloat(newDepositeAmountText)
 
     const depTotal = document.getElementById('deposite-total')
+    const previousDepositeAmountText = depTotal.innerText
+    const previousDepositeAmount = parseFloat(previousDepositeAmountText)
 
-    depTotal.innerText =  depositeAmount
+
+
+    const newDepTotal = previousDepositeAmount + newDepositeAmount 
+
+    depTotal.innerText =  newDepTotal
+
+
+    
+
+    // update total balance
+
+    const balanceTotal = document.getElementById('balance-total')
+    // console.log('jj')
+
+    const balanceTotalText = balanceTotal.innerText
+    const previousBalanceTotal = parseFloat(balanceTotalText)
+    console.log(balanceTotalText)
+
+    const newBalanceTotal = previousBalanceTotal + newDepositeAmount
+    // console.log(newBalanceTotal)
+
+    balanceTotal.innerText = newBalanceTotal
+
+
 
     // clear the deposite input value
 
     depositeInput.value = 0
-    
+
 })
+
+
